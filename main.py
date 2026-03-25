88ffa6c9df446a2d758e47d09a18006d4b1bcd9f
PrashantVIT-25 <prashant.25bai11147@vitbhopal.ac.in>
2026-03-26 00:31:25 +0530

    Efficient 4th degree curve model addition

--- main.py
+++ main.py
@@ -16,3 +16,13 @@ y = df['Exam_Score']
 
 # 3. Data Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 4. --- EFFICIENCY UPGRADE: MODEL TRAINING---
+# Model : Degree 4
+poly_4 = PolynomialFeatures(degree=4)
+X_train_p4 = poly_4.fit_transform(X_train)
+X_test_p4 = poly_4.transform(X_test)
+
+model_4 = LinearRegression().fit(X_train_p4, y_train)
+p4_pred = model_4.predict(X_test_p4)
+
