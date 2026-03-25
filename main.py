4bf2ad231afdf712ee2a096170010a64697deb03
PrashantVIT-25 <prashant.25bai11147@vitbhopal.ac.in>
2026-03-26 00:26:42 +0530

    Features, Target and Data Split

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # 1. Data load
 df = pd.read_csv("StudentPerformanceFactors.csv").dropna()
+
+# 2. Features and Target
+features = ['Hours_Studied', 'Attendance', 'Previous_Scores', 'Tutoring_Sessions']
+X = df[features]
+y = df['Exam_Score']
+
+# 3. Data Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
